76280771ecde6cb7f16705f896b2371f9b4f5ef0
Won Young Chi <wonyoung.chi@gmail.com>
2020-05-21 08:21:51 +0200

    clear chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+fn usage() {
+    let the_version = get_version();
+    /*
+    let the_version: u16;   // Declare our variable
+    the_version = get_version();    // Assign a value to our variable
+    */
+    println!("tinymd, a markdown compiler written by Wony.");
+    println!("Version {}", the_version);  // Print the value assigned
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
+    get_version();
+}
+
+fn get_version() ->u16 {
+    1000
 }
