4d86e7da92d7376af4493c96ab04e71e47f69944
Won Young Chi <wonyoung.chi@gmail.com>
2020-05-21 11:32:24 +0200

    clear chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
+fn parse_markdown_file() {
+
+}
+
+fn print_short_banner() {
+    println!("{}", get_title());
+}
+
+fn print_long_banner() {
+    print_short_banner();
+    let mut written_by = String::from("Written by: ");
+    written_by.push_str(env!("CARGO_PKG_AUTHORS"));
+    let mut homepage = String::from("Homepage: ");
+    homepage.push_str(env!("CARGO_PKG_HOMEPAGE"));
+    let usage = String::from("Usage: tinymd <somefile>.md");
+    println!("{}", written_by);
+    println!("{}", homepage);
+    println!("{}", usage);
+}
+
+fn get_title() -> String {
+    let mut the_title = String::from(env!("CARGO_PKG_NAME"));
+    the_title.push_str(" (v");
+    the_title.push_str(env!("CARGO_PKG_VERSION"));
+    the_title.push_str("), ");
+    the_title.push_str(&String::from(env!("CARGO_PKG_DESCRIPTION")));
+
+    the_title
+}
+
 fn usage() {
-    let the_version = get_version();
-    /*
-    let the_version: u16;   // Declare our variable
-    the_version = get_version();    // Assign a value to our variable
-    */
-    println!("tinymd, a markdown compiler written by Wony.");
-    println!("Version {}", the_version);  // Print the value assigned
+    print_long_banner();
 }
 
 fn main() {
     usage();
-    get_version();
-}
-
-fn get_version() ->u16 {
-    1000
 }
